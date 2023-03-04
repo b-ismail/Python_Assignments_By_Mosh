fe709310cb9a06e7aa0ca9c18a6b4915ac82d418
b-ismail <bilalahmedismail@gmail.com>
2023-03-04 18:17:30 +0500

    Uptil weight lbs to kg activity

---
+++ 06_type_conversion.py
@@ -0,0 +1,7 @@
+birth_year = input('Birth Year: ')
+age = 2019 - int(birth_year)
+# int()
+# float()
+# bool()
+print(type(age))
+print(age)
---
+++ 07_Activity_weight.py
@@ -0,0 +1,3 @@
+weight_lbs = input('Enter your weight in pounds lbs. ')
+weight_kgs = float(weight_lbs) * (0.453592)
+print(weight_kgs)
