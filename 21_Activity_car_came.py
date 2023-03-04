bbe0f791939e9d78f3b0e5eea299a26cc5dae68a
b-ismail <bilalahmedismail@gmail.com>
2023-03-04 22:33:02 +0500

    updated, car game

--- 21_Activity_car_came.py
+++ 21_Activity_car_came.py
@@ -1,17 +1,28 @@
 choice = ''
+is_started = False
 help_message = '''
+
 start - to start the car
 stop - to stop the car
 quit - to exit
+
 '''
 while choice.lower() != 'quit':
-    choice = input()
+    choice = input("> ")
     if choice.lower() == 'help':
         print(help_message)
     elif choice.lower() == 'start':
-        print('Car started....Ready to go!')
+        if is_started:
+            print("Already started!")
+        else:
+            is_started = True
+            print('Car started....Ready to go!')
     elif choice.lower() == 'stop':
-        print('Car stopped!')
+        if is_started:
+            is_started = False
+            print('Car stopped!')
+        else:
+            print('Car already stopped!')
     else:
         print("I don't understand that...")
 else :
