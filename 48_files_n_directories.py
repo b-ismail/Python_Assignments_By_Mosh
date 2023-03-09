c09f410d694c3b4815eef88a7a6a186c7fd16027
b-ismail <bilalahmedismail@gmail.com>
2023-03-09 11:57:49 +0500

    file system manipulations

--- 48_files_n_directories.py
+++ 48_files_n_directories.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 
 # Absolute, from root of Hard, "c:\" or "/" in linux
+
+
 dir_path = Path("45_packages\email")
 try:
     print(dir_path.mkdir())
 except:
     print(dir_path.rmdir())
+
+dir_path = Path("45_packages")
+for file in dir_path.glob("*"):
+# for file in dir_path.glob("*.*"):
+    print(file)
+
+
 
 # Relative paath
