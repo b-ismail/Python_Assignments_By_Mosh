e1a9d8914610e3008f370b282f9b05e63e059b76
b-ismail <bilalahmedismail@gmail.com>
2023-03-12 13:13:29 +0500

    working woth spreadsheets

--- 49_spreadsheets_manipulation/spreadsheets_app.py
+++ 49_spreadsheets_manipulation/spreadsheets_app.py
@@ -1,9 +1,17 @@
 import openpyxl as xl
 
-xl.load_workbook('transactions.xlsx')
+# root_path
+wb=xl.load_workbook('S:\\Challenge_JH_Tasks\\12_SkipQ\\Python_Assignments_By_Mosh\\49_spreadsheets_manipulation\\trans.xlsx')
+# wb=xl.load_workbook('\\trans.xlsx')
 sheet = wb['Sheet1']
 
 cell = sheet['a1']
 #  OR
-cell = sheet.cell(1, 1)
-print(cell.value)
+for ind in range(1,sheet.max_row+1):
+    cell = sheet.cell(ind, 3) # row and column
+    # print(cell.value)
+    corrected_value = int(cell.value) * 0.9
+    corrected_price_cell = sheet.cell(ind, 4)
+    corrected_price_cell = corrected_value
+
+wb.save('transactions2.xlsx')
